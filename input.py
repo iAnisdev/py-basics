6af88aa7324a6f3665f894c2b560ef0560899182
Anis <imarahmandev@gmail.com>
2024-02-10 01:08:22 +0700

    type conversion

--- input.py
+++ input.py
@@ -1,3 +1,15 @@
 first_name = input("What is your first name? ")
 last_name = input("What is your last name? ")
 print("Hello " + first_name + " " + last_name + "!")
+
+
+first_number = input("Enter a number: ")
+second_number = input("Enter another number: ")
+sum = int(first_number) + int(second_number)
+print("The sum of " + first_number + " and " + second_number + " is " + str(sum) + ".")
+
+
+first_number = input("Enter a floating number: ")
+second_number = input("Enter another floating number: ")
+sum = float(first_number) + float(second_number)
+print("The sum of " + first_number + " and " + second_number + " is " + str(sum) + ".")
