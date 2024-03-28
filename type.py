f4a0eda58515a3d067a1fae6ea98abc99825b30b
Anis <anis.rahman@emma-sleep.com>
2024-03-28 11:12:59 +0700

    Type conversion

---
+++ type.py
@@ -0,0 +1,20 @@
+name = "A"
+age = 20
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+
+age = float(age)
+print(age)
+print(type(age))
+is_student = str(is_student)
+print(is_student)
+print(type(is_student))
+is_student = bool(is_student)
+print(is_student)
+print(type(is_student))
+is_student = int(is_student)
+print(is_student)
+print(type(is_student))
