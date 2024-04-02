c1ed41b73cb3f52177a7c377a5e6bac56f86743d
Anis <imarahmandev@gmail.com>
2024-04-02 23:51:16 +0700

    String slice

---
+++ slice.py
@@ -0,0 +1,9 @@
+sentence = 'The quick brown fox jumps over the lazy dog.'
+
+print(sentence[0:3]) 
+print(sentence[4:9])
+print(sentence[10:15])
+print(sentence[0:10:1])
+print(sentence[0:10:2])
+print(sentence[0:10:3])
+print(sentence[::-1])
